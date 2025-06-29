c3dad8d1735a32d3ffbbf4d29d278a96b0c29ad6
Muhammad Ahmad <149201186+m-1Ahmad@users.noreply.github.com>
2025-06-29 20:23:16 +0500

    Update settings.py

--- backend/backend_core/settings.py
+++ backend/backend_core/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mahmad0449939@gmail.com'
-EMAIL_HOST_PASSWORD = 'wdsssfuutjegzksu'
+EMAIL_HOST_USER = 'test@gmail.com' #gmail account
+EMAIL_HOST_PASSWORD = 'samplecode' #16 digit code after 2fa in gmail
